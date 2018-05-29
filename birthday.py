0ad71f11cbf19624d48035f0a17b996e8217c413
Justice0320 <garrycraig@gmail.com>
2018-05-29 20:59:59 +0100

    number and string practice
    Signed-off-by: garry <garrycraig@gmail.com>

---
+++ birthday.py
@@ -0,0 +1,15 @@
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+# number practice
+
+print(4 + 4)
+print(12 - 4)
+print(4 * 2)
+print(80 / 10)
+
+fav_numb = 20
+message = "My favourite number is: " + str(fav_numb)
+
+print(message)
