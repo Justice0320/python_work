fd0d8c6b689c48f28c19a2428fb283e405b13cfa
Justice0320 <garrycraig@gmail.com>
2018-05-29 20:39:32 +0100

    Commit name lesson and strip
    Signed-off-by: garry <garrycraig@gmail.com>

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+# examples of syntax errors caused by " and ' misuse
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+message = 'One of Python's strengths is its diverse community.'
+print(message)
