bc26dcd9a3e4c2b30cfe607b368f7a0b262ac03e
Justice0320 <garrycraig@gmail.com>
2018-05-30 21:08:40 +0100

    Chapter 3 list practice - exercises and tasks+ list ordering practice

---
+++ ch3/seeing_the_world.py
@@ -0,0 +1,23 @@
+places_to_visit = ["borneo", "pakistan", "cuba", "america", "norway"]
+print(places_to_visit)
+
+print(sorted(places_to_visit))
+
+print(places_to_visit)
+
+reversed_alpha = sorted(places_to_visit)
+print(sorted(reversed_alpha, reverse=True))
+
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
+
+places_to_visit.sort()
+print(places_to_visit)
+
+places_to_visit.reverse()
+print(places_to_visit)
