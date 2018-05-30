3f7f473bd183d6c5e955c36b8be73eb65e8275f5
Justice0320 <garrycraig@gmail.com>
2018-05-30 19:38:55 +0100

    Chapter 3 list practice - fix corrupt index file

---
+++ ch3/cars.py
@@ -0,0 +1,13 @@
+cars = ["ford", "honda", "Lamborghini", "McLaren"]
+
+message = "My next car will be a " + cars[0] + "."
+print(message)
+
+message = "My next car will be a " + cars[1] + "."
+print(message)
+
+message = "My next car will be a " + cars[2] + "."
+print(message)
+
+message = "My next car will be a " + cars[3] + "."
+print(message)
---
+++ ch3/names.py
@@ -0,0 +1,16 @@
+names = ["garry", "misha", "chris", "gareth", "martin", "tara"]
+
+message = "Hi " + (names[1].title()) + ", Nice to see you."
+print(message)
+
+message = "Hi " + (names[2].title()) + ", Nice to see you."
+print(message)
+
+message = "Hi " + (names[3].title()) + ", Nice to see you."
+print(message)
+
+message = "Hi " + (names[4].title()) + ", Nice to see you."
+print(message)
+
+message = "Hi " + (names[5].title()) + ", Nice to see you."
+print(message)
